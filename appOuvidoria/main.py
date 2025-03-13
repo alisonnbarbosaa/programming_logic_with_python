3c87d8ba06f037c247430c2bd98b09033d123e3a
alisonnbarbosaa <alisondevbarbosa@gmail.com>
2025-03-13 01:15:41 -0300

    fixing bug in choosing options

--- appOuvidoria/main.py
+++ appOuvidoria/main.py
@@ -5,14 +5,26 @@ elogios = []
 while True:
 
     print("Lista de serviços:\n\n1) Enviar uma reclamação.\n2) Enviar uma sugestão.\n3) Enviar um elogio.\n4) Sair do sistema.\n")
-    servico = int(input("Escolha um serviço: "))
+    servico = input("Escolha um serviço: ")
+
+    if not servico.isdigit():
+        print("\nServiço inválido. Tente novamente!\n")
+        continue
+
+    servico = int(servico)
 
     if servico == 1:
         # Trabalha com a lista de reclamações:
         while True:
-            print("\nLista de serviços:\n\n1) Listar as Manifestações.\n2) Criar uma nova Manifestação.\n3) Exibir quantidade de manifestações.\n4) Pesquisar uma manifestação por código.\n5) Sair do serviço.\n")
+            print("Lista de serviços:\n\n1) Listar as Manifestações.\n2) Criar uma nova Manifestação.\n3) Exibir quantidade de manifestações.\n4) Pesquisar uma manifestação por código.\n5) Sair do serviço.\n")
 
-            opcao = int(input("Informe sua opção: "))
+            opcao = input("Informe sua opção: ")
+
+            if not opcao.isdigit():
+                print("\nServiço inválido. Tente novamente!\n")
+                continue
+
+            opcao = int(opcao)
 
             if opcao == 1:
                 print("Listar as Manifestações")
@@ -31,7 +43,7 @@ while True:
 
         # Trabalha com a lista de sugestões:
         while True:
-            print("\nLista de serviços:\n\n1) Listar as Manifestações.\n2) Criar uma nova Manifestação.\n3) Exibir quantidade de manifestações.\n4) Pesquisar uma manifestação por código.\n5) Sair do serviço.\n")
+            print("Lista de serviços:\n\n1) Listar as Manifestações.\n2) Criar uma nova Manifestação.\n3) Exibir quantidade de manifestações.\n4) Pesquisar uma manifestação por código.\n5) Sair do serviço.\n")
 
             opcao = int(input("Informe sua opção: "))
 
@@ -52,7 +64,7 @@ while True:
 
         # Trabalha com a lista de elogios:
         while True:
-            print("\nLista de serviços:\n\n1) Listar as Manifestações.\n2) Criar uma nova Manifestação.\n3) Exibir quantidade de manifestações.\n4) Pesquisar uma manifestação por código.\n5) Sair do serviço.\n")
+            print("Lista de serviços:\n\n1) Listar as Manifestações.\n2) Criar uma nova Manifestação.\n3) Exibir quantidade de manifestações.\n4) Pesquisar uma manifestação por código.\n5) Sair do serviço.\n")
 
             opcao = int(input("Informe sua opção: "))
 
